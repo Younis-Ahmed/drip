891265d84741f0c30d959db3eeaddd7c560cc01b
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-05 13:51:10 +0300

    Refactor authentication providers in auth.ts

--- server/auth.ts
+++ server/auth.ts
@@ -1,20 +1,30 @@
-import NextAuth from "next-auth"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "@/server"
-import Google from "next-auth/providers/google"
-import Github from "next-auth/providers/github"
- 
+import NextAuth from 'next-auth';
+import dotenv from 'dotenv';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { db } from '@/server';
+import Google from 'next-auth/providers/google';
+import Twitter from 'next-auth/providers/twitter';
+
+
+dotenv.config({
+  path: '.env.local',
+});
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
+  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: 'jwt'
+  },
   providers: [
     // Providers
     Google({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    Github({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
-    })
+    Twitter({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    }),
   ],
-})
+});
