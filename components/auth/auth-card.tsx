6b4578fc8581aadb501397c191184cb25d3c7166
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-05 16:02:36 +0300

    Add AuthCard component to handle authentication card rendering

---
+++ components/auth/auth-card.tsx
@@ -0,0 +1,18 @@
+
+type CardWrapperProps = {
+    children: React.ReactNode;
+    cardTitle: string;
+    backBtnHref: string;
+    backBtnLabel: string;
+    showSocials?: boolean;
+};
+
+export const AuthCard:  = ({
+    children,
+    cardTitle,
+    backBtnHref,
+    backBtnLabel,
+    showSocials,
+}: CardWrapperProps) => {
+    
+}
