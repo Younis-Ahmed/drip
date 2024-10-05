dced081735cee2a6fd5e1d743c94d75609012923
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-05 13:50:35 +0300

    Refactor database configuration in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,14 +4,15 @@ import * as dotenv from 'dotenv';
 dotenv.config({
   path: ".env.local",
 });
+
 export default defineConfig({
   schema: './server/schema.ts',
   out: './server/migrations',
   dialect: 'postgresql', // 'postgresql' | 'mysql' | 'sqlite'
   dbCredentials: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'test',
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
   },
 });
