1637ba2a4a882dfb6654d710e4e4e039cf0d08c3
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-06 17:01:43 +0300

    Refactor layout component to add responsive padding and max width

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased px-6 md:px-12 max-w-7xl mx-auto`}
       >
         <Nav />
         {children}
