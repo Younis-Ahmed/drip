f62ae8dd0b81ca5c211561ff53860166fd6d0ba3
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-08 13:04:41 +0300

    Refactor email sign-in action to check for existing user and implement email verification

--- server/actions/email-signin.ts
+++ server/actions/email-signin.ts
@@ -1,12 +1,30 @@
 'use server';
 import { loginSchema } from '@/types/login-schema';
 import { createSafeActionClient } from 'next-safe-action';
+import { db } from '..';
+import { eq } from 'drizzle-orm';
+import { users } from '@/server/schema';
 
 const actionController = createSafeActionClient();
 
 export const emailSignIn = actionController
   .schema(loginSchema)
   .action(async ({ parsedInput: { email, password, code } }) => {
-    console.log(email, password, code);
-    return { email, password, code };
+    // Check if user exists in the database
+    const existingUser = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    });
+
+    if (existingUser?.email !== email) {
+      return { error: 'User not found' };
+    }
+
+    // Check if user has verified their email [TODO] - Implement email verification
+    
+    // if (!existingUser?.emailVerified) {
+
+    // }
+
+    console.info(existingUser);
+    return { success: email };
   });
