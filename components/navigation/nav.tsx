b948a6f706f51a4b97d3ba46e3e71e7265c6ac8b
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-08 10:51:01 +0300

    Refactor navigation component to add logo and improve UI layout

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -3,16 +3,22 @@ import { UserBtn } from '@/components/navigation/userBtn';
 import { Button } from '../ui/button';
 import Link from 'next/link';
 import { LogIn } from 'lucide-react';
+import Logo from '@/assets/images/logo.webp';
+import Image from 'next/image';
 
 export default async function Nav() {
   const session = await auth();
   console.log(session);
 
   return (
-    <header className=' py-8'>
+    <header className='py-8'>
       <nav>
         <ul className='flex justify-between'>
-          <li>Logos</li>
+          <li>
+            <Link href='/'>
+              <Image src={Logo} alt='Logo' className='size-16' />
+            </Link>
+          </li>
           {!session ? (
             <li>
               <Button asChild>
