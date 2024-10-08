29c3ef152f04119797b7a86ce5b1197952078ac8
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-08 09:54:14 +0300

    Refactor AuthCard component to use Card component for consistent styling and layout

--- components/auth/auth-card.tsx
+++ components/auth/auth-card.tsx
@@ -1,18 +1,36 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import Socials from './socials';
+import BackButton from './back-button';
 
 type CardWrapperProps = {
-    children: React.ReactNode;
-    cardTitle: string;
-    backBtnHref: string;
-    backBtnLabel: string;
-    showSocials?: boolean;
+  children: React.ReactNode;
+  cardTitle: string;
+  backBtnHref: string;
+  backBtnLabel: string;
+  showSocials?: boolean;
 };
 
-export const AuthCard:  = ({
-    children,
-    cardTitle,
-    backBtnHref,
-    backBtnLabel,
-    showSocials,
+export const AuthCard = ({
+  children,
+  cardTitle,
+  backBtnHref,
+  backBtnLabel,
+  showSocials,
 }: CardWrapperProps) => {
-    
-}
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{cardTitle}</CardTitle>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocials && (
+        <CardFooter>
+          <Socials />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton label={backBtnLabel} href={backBtnHref} />
+      </CardFooter>
+    </Card>
+  );
+};
