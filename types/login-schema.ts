cc65ddcac3e44b6bfda80beb13170d67337f95ff
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-11 15:02:36 +0300

    Update password validation to require a minimum of 8 characters

--- types/login-schema.ts
+++ types/login-schema.ts
@@ -4,7 +4,7 @@ export const loginSchema = z.object({
   email: z.string().email({
     message: 'Invalid email address',
   }),
-  password: z.string().min(6, {
+  password: z.string().min(8, {
     message: 'Password must be at least 6 characters long',
   }),
   code: z.optional(z.string()),
