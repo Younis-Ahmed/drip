cdf3e4b086821faebf92762488e953a27be684a0
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-11 15:01:34 +0300

    Refactor form-success.tsx to rename formError component to FormSuccess

--- components/auth/form-success.tsx
+++ components/auth/form-success.tsx
@@ -1,10 +1,10 @@
 import { CheckCircle2 } from 'lucide-react';
 
-export const formError = ({ message }: { message?: string }) => {
+export const FormSuccess = ({ message }: { message?: string }) => {
   if (!message) return null;
 
   return (
-    <div className='rounded-md bg-teal-400 p-3 text-secondary-foreground'>
+    <div className='rounded-md bg-green-400/30 my-2 text-xs font-medium p-3 text-secondary-foreground flex items-center gap-2'>
       <CheckCircle2 className='mr-2 h-5 w-5' />
       <p>{message}</p>
     </div>
