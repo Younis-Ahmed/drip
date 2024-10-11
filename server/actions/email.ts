5c2167dd476a9914d7937125cb43fae7548f4c09
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-11 13:47:41 +0300

    Refactor email.ts to send verification email with confirmation link

---
+++ server/actions/email.ts
@@ -0,0 +1,26 @@
+"use server"
+
+import { EmailTemplate } from "@/components/ui/email-template";
+import getBaseURL from "@/lib/base-url";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESNED_API_KEY);
+const domain = getBaseURL();
+
+export const sendVerficationEmail = async (email: string, token: string) => {
+
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+    const { data, error } = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: ['delivered@resend.dev'],
+        subject: 'Drip - Confirmation Email',
+        react: EmailTemplate({ confirmLink }),
+      });
+    
+      if (error) {
+        return console.error(error);
+      }
+    
+      return data;
+
+};
