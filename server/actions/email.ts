837e0ac94bdf064533f0d69ddbeb4fb2fd3f07f2
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-19 15:59:48 +0300

    Refactor email.ts to handle verification token generation and email sending errors

--- server/actions/email.ts
+++ server/actions/email.ts
@@ -1,26 +1,24 @@
-"use server"
+'use server';
 
-import { EmailTemplate } from "@/components/ui/email-template";
-import getBaseURL from "@/lib/base-url";
-import { Resend } from "resend";
+import { EmailTemplate } from '@/components/ui/email-template';
+import getBaseURL from '@/lib/base-url';
+import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESNED_API_KEY);
 const domain = getBaseURL();
 
 export const sendVerficationEmail = async (email: string, token: string) => {
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+  const { data, error } = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: process.env.EMAIL!,
+    subject: 'Drip - Confirmation Email',
+    react: EmailTemplate({ confirmLink }),
+  });
 
-    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
-    const { data, error } = await resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: [email],
-        subject: 'Drip - Confirmation Email',
-        react: EmailTemplate({ confirmLink }),
-      });
-    
-      if (error) {
-        return console.error(`${error} from email.ts line 41`);
-      }
-    
-      return data;
+  if (error) {
+    return console.error(JSON.stringify(error), `from email.ts line 41`);
+  }
 
+  return data;
 };
