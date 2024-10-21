a8fba4be5ce459efd76fbf66e30a05bc652342e2
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-21 14:40:27 +0300

    Refactor email-signin.ts to handle email verification and sign-in process

--- server/actions/email-signin.ts
+++ server/actions/email-signin.ts
@@ -4,6 +4,10 @@ import { createSafeActionClient } from 'next-safe-action';
 import { db } from '..';
 import { eq } from 'drizzle-orm';
 import { users } from '@/server/schema';
+import { generateEmailVerificationToken } from './tokens';
+import { sendVerficationEmail } from './email';
+import { signIn } from 'next-auth/react';
+import { AuthError } from 'next-auth';
 
 const actionController = createSafeActionClient();
 
@@ -11,21 +15,50 @@ export const emailSignIn = actionController
   .schema(loginSchema)
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   .action(async ({ parsedInput: { email, password, code } }) => {
-    // Check if user exists in the database
-    const existingUser = await db.query.users.findFirst({
-      where: eq(users.email, email),
-    });
+    try {
+      // Check if user exists in the database
+      const existingUser = await db.query.users.findFirst({
+        where: eq(users.email, email),
+      });
 
-    if (existingUser?.email !== email) {
-      return { error: 'User not found' };
+      if (existingUser?.email !== email) {
+        return { error: 'User not found' };
+      }
+
+      // Check if user has verified their email [TODO] - Implement email verification
+
+      if (!existingUser?.emailVerified) {
+        const verificationToken = await generateEmailVerificationToken(existingUser.email);
+        if (verificationToken && verificationToken.length > 0) {
+          await sendVerficationEmail(verificationToken[0].email, verificationToken[0].token);
+          return { success: 'Verification email sent' };
+        } else {
+          return { error: 'Failed to generate verification token' };
+        }
+      }
+
+      await signIn('credentials', {
+        email,
+        password,
+        redirectTo: '/',
+      });
+
+      console.info(existingUser);
+      return { success: 'User Signed In!' };
+    } catch (e) {
+      console.error(e);
+      if (e instanceof AuthError) {
+        switch (e.type) {
+          case 'OAuthSignInError':
+            return { error: e.message };
+          case 'OAuthCallbackError':
+            return { error: e.message };
+          case 'CredentialsSignin':
+            return { error: e.message };
+          default:
+            return { error: 'Something went wrong' };
+        }
+      }
+      throw e;
     }
-
-    // Check if user has verified their email [TODO] - Implement email verification
-    
-    // if (!existingUser?.emailVerified) {
-
-    // }
-
-    console.info(existingUser);
-    return { success: email };
   });
