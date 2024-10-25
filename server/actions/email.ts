0408fb2848eba4e0fbcd346113cdabab5da7066f
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-25 14:11:46 +0300

    Refactor email actions to include dynamic title and link content

--- server/actions/email.ts
+++ server/actions/email.ts
@@ -11,9 +11,33 @@ export const sendVerficationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
   const { data, error } = await resend.emails.send({
     from: 'Acme <onboarding@resend.dev>',
+    to: process.env.EMAIL || email,
+    subject: 'Drip - Confirmation Email',
+    react: EmailTemplate({
+      confirmLink,
+      title: 'Hi There',
+      linkContent: 'Click here to confirm your email address',
+    }),
+  });
+
+  if (error) {
+    return console.error(JSON.stringify(error), `from email.ts line 41`);
+  }
+
+  return data;
+};
+
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const confirmLink = `${domain}/auth/new-password?token=${token}`;
+  const { data, error } = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
     to: process.env.EMAIL!,
     subject: 'Drip - Confirmation Email',
-    react: EmailTemplate({ confirmLink }),
+    react: EmailTemplate({
+      confirmLink,
+      title: 'Hi There',
+      linkContent: 'Click here to reset your password',
+    }),
   });
 
   if (error) {
