4eb422ced010777e2948c405c939b8d587104679
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-27 16:55:55 +0300

    Add SettingsCard component to Dashboard settings

---
+++ app/dashboard/settings/settings-card.tsx
@@ -0,0 +1,42 @@
+'use client';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Session } from 'next-auth';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+  import { Input } from "@/components/ui/input" 
+
+function SettingsCard(session: Session) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Your Settings</CardTitle>
+        <CardDescription>Update Your account settings</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default SettingsCard;
