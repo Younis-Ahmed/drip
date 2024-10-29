2c30b3cf00eeedee4ed65d99316b0e8508215320
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-29 17:45:58 +0300

    Refactor AddProduct component to improve layout and add product description

--- app/dashboard/add-product/page.tsx
+++ app/dashboard/add-product/page.tsx
@@ -1,3 +1,8 @@
 export default function AddProduct() {
-  return <div>Add Product</div>;
+  return (
+    <div className='flex h-full flex-col items-center justify-center space-y-4'>
+      <h1 className='text-2xl font-semibold'>Add Product</h1>
+      <p className='text-center text-sm text-muted'>Add a new product to your store</p>
+    </div>
+  );
 }
