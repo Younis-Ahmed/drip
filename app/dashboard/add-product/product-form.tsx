26fef55fc2f8e74ece8311ecce39063b91e67718
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-10-30 12:54:25 +0300

    Refactor AddProduct component to include a form for adding new products

---
+++ app/dashboard/add-product/product-form.tsx
@@ -0,0 +1,92 @@
+'use client';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardFooter,
+  CardDescription,
+  CardTitle,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { zProductSchema } from '@/types/product-schema';
+import { DollarSign } from 'lucide-react';
+
+function ProductForm() {
+  const form = useForm<zProductSchema>({
+    defaultValues: {
+      title: '',
+      price: 0,
+      description: '',
+    },
+  });
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+            <FormField
+              control={form.control}
+              name='title'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder='Your Title' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='description'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>{/* <Input placeholder='Your Title' {...field} /> */}</FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='description'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Price</FormLabel>
+                  <FormControl>
+                    <div className='flex items-center gap-2'>
+                      <DollarSign size={36} className='rounded-md bg-muted p-2' />
+                      <Input type='number' placeholder='Your price in USD' step={0.1} min={0} {...field} />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='w-full' type='submit'>Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default ProductForm;
