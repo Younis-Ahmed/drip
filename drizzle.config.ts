ba6d90a3c4c3ff8a702490192b6d46324fb5fe30
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-11-02 17:32:06 +0300

    Update drizzle.config.ts to enable verbose logging

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
 
 dotenv.config({
-  path: ".env.local",
+  path: '.env.local',
 });
 
 export default defineConfig({
   schema: './server/schema.ts',
   out: './server/migrations',
   dialect: 'postgresql', // 'postgresql' | 'mysql' | 'sqlite'
+  verbose: true,
   dbCredentials: {
     host: process.env.DB_HOST!,
     user: process.env.DB_USER!,
