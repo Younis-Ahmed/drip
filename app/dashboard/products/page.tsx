d1343ac0331a9910a6d2f00588d6ab7aec8cf58e
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-11-03 17:36:40 +0300

    Refactor Products component to fetch and display product data in DataTable

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,7 +1,29 @@
+import { db } from '@/server';
 import React from 'react';
+import Placeholder from '@/assets/images/placeholder.png';
+import { DataTable } from './data-table';
+import { columns } from './columns';
+async function Products() {
+  const products = await db.query.products.findMany({
+    orderBy: (products, { desc }) => [desc(products.id)],
+  });
+  if (!products) throw new Error('No products found');
 
-function Product() {
-  return <div>Product</div>;
+  const dataTable = products.map(product => {
+    return {
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      variants: [],
+      image: Placeholder.src,
+    };
+  });
+  if (!dataTable) throw new Error('No data found');
+  return (
+    <div>
+      <DataTable columns={columns} data={dataTable} />
+    </div>
+  );
 }
 
-export default Product;
+export default Products;
