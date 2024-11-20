0a790036f96329e9482fcef599058edbd0edd4cb
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-11-20 17:03:23 +0300

    Fetch product variants from the database and render them in the Products component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-
+import Products from '@/components/products/products';
+import { db } from '@/server';
 
 export default async function Home() {
-
-    return (
-      <main>
-        <h1>HEY</h1>
-      </main>
-    );
-  }
-
+  const data = await db.query.productVariants.findMany({
+    with: {
+      variantsImages: true,
+      variantsTags: true,
+      product: true,
+    },
+    orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
+  });
+  return (
+    <main className=''>
+      <Products variants={data} />
+    </main>
+  );
+}
