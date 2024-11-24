6ab09cfea2ebdad52651f8afc4e6de2fbbbed520
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-11-24 17:43:59 +0300

    Add ProductCarousal component to product page for enhanced image display

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import ProductPicks from '@/components/products/product-picks';
 import ProductType from '@/components/products/product-type';
+import ProductCarousal from '@/components/products/products-carousal';
 import { Separator } from '@/components/ui/separator';
 import formatPrice from '@/lib/format-price';
 import { db } from '@/server';
@@ -39,34 +40,32 @@ export default async function Page({ params }: { params: { slug: string } }) {
   if (variant) {
     return (
       <main>
-         <section>
+        <section className='flex flex-col lg:flex-row gap-4 lg:gap-12'>
           <div className='flex-1'>
-            <h1>images</h1>
+            <ProductCarousal variants={variant.product.productVariants} />
           </div>
-          <div className='flex flex-1 flex-col gap-2'>
-            <h2 className=''>{variant?.product?.title}</h2>
+          <div className='flex flex-1 flex-col '>
+            <h2 className='text-2xl font-bold'>{variant?.product?.title}</h2>
             <div>
               <ProductType variants={variant.product.productVariants} />
             </div>
           </div>
-          <Separator />
-          <p className='text-2xl font-medium'>
-            {formatPrice(variant.product.price)}
-          </p>
-          <div dangerouslySetInnerHTML={{__html: variant.product.description}}></div>
-          <p className='text-secondary-foreground'>Available Variants</p>
-          <div className='flex gap-4 '>
+          <Separator className='my-2' />
+          <p className='text-2xl font-medium py-2'>{formatPrice(variant.product.price)}</p>
+          <div dangerouslySetInnerHTML={{ __html: variant.product.description }}></div>
+          <p className='text-secondary-foreground font-medium my-2'>Available Variants</p>
+          <div className='flex gap-4'>
             {variant.product.productVariants.map(prodVariant => (
-                <ProductPicks
-                  key={prodVariant.id}
-                  id={prodVariant.id}
-                  color={prodVariant.color}
-                  productType={prodVariant.productType}
-                  title={variant.product.title}
-                  price={variant.product.price}
-                  productID={variant.productID}
-                  image={prodVariant.variantsImages[0].url}
-                />
+              <ProductPicks
+                key={prodVariant.id}
+                id={prodVariant.id}
+                color={prodVariant.color}
+                productType={prodVariant.productType}
+                title={variant.product.title}
+                price={variant.product.price}
+                productID={variant.productID}
+                image={prodVariant.variantsImages[0].url}
+              />
             ))}
           </div>
         </section>
