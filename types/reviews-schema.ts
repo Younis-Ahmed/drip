7ad1cf40183464b89b23860ae9721361f3e9e050
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-11-29 16:45:33 +0300

    Add productID and comment fields to reviews schema with validation

--- types/reviews-schema.ts
+++ types/reviews-schema.ts
@@ -1,7 +1,15 @@
 import * as z from 'zod';
 
 export const reviewsSchema = z.object({
-    rating: z.number().int().min(1, {message: 'Please add at least one star'}).max(5, {message: 'Please add at most five stars'}),
-    title: z.string().min(10, { message: 'Please add at least 10 characters for this review'}).max(100),
-    // body: z.string().min(1).max(500),
-})
+  productID: z.number().int(),
+  rating: z
+    .number()
+    .int()
+    .min(1, { message: 'Please add at least one star' })
+    .max(5, { message: 'Please add at most five stars' }),
+  title: z
+    .string()
+    .min(10, { message: 'Please add at least 10 characters for this review' })
+    .max(100),
+  comment: z.string().min(50, { message: 'Please add at least 50 characters for this review' }),
+});
