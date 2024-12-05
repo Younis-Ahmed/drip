5255127c7c864e852f77f849bafbbbc5c73e68e5
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-05 09:14:52 +0300

    Update Products component to handle products without variants and set appropriate image source

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -14,12 +14,23 @@ async function Products() {
   if (!products) throw new Error('No products found');
 
   const dataTable = products.map(product => {
+    if (product.productVariants.length === 0) {
+      return {
+        id: product.id,
+        title: product.title,
+        price: product.price,
+        image: Placeholder.src,
+        variants: [],
+      };
+    }
+    const image = product.productVariants[0].variantsImages[0].url;
+
     return {
       id: product.id,
       title: product.title,
       price: product.price,
       variants: product.productVariants,
-      image: Placeholder.src,
+      image,
     };
   });
   if (!dataTable) throw new Error('No data found');
