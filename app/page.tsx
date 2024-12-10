4feda85d1cd57287fea9a6087c68230680b13c5e
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-10 15:58:52 +0300

    Refactor page component: remove unnecessary semicolons and improve formatting for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Products from '@/components/products/products';
-import { db } from '@/server';
+import Products from '@/components/products/products'
+import { db } from '@/server'
 
 export default async function Home() {
   const data = await db.query.productVariants.findMany({
@@ -9,10 +9,10 @@ export default async function Home() {
       product: true,
     },
     orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
-  });
+  })
   return (
-    <main className=''>
+    <main className="">
       <Products variants={data} />
     </main>
-  );
+  )
 }
