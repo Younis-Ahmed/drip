4273bb9db6a0e3183cc30a3f8719500249b0b8ce
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-10 15:54:36 +0300

    Add initial ESLint configuration file for project

---
+++ app/.eslintrc.js
@@ -0,0 +1,19 @@
+// // .eslintrc.js
+// module.exports = {
+//   extends: ['@antfu'],
+//   parserOptions: {
+//     ecmaVersion: 2020,
+//     sourceType: 'module',
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//   },
+//   env: {
+//     browser: true,
+//     node: true,
+//     es2021: true,
+//   },
+//   rules: {
+//     // Add any project-specific ESLint rules here
+//   },
+// };
