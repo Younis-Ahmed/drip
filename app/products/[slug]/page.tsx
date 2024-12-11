4fc98746096aef880e92b541662d0cfd94ed213f
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-11 17:26:05 +0300

    Refactor page component: standardize import quotes and improve formatting

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,14 +1,14 @@
-import ProductPicks from '@/components/products/product-picks';
-import ProductType from '@/components/products/product-type';
-import ProductCarousal from '@/components/products/products-carousal';
-import Reviews from '@/components/reviews/reviews';
-import Stars from '@/components/reviews/stars';
-import { Separator } from '@/components/ui/separator';
-import formatPrice from '@/lib/format-price';
-import { getReviewAverage } from '@/lib/review-average';
-import { db } from '@/server';
-import { productVariants } from '@/server/schema';
-import { eq } from 'drizzle-orm';
+import ProductPicks from '@/components/products/product-picks'
+import ProductType from '@/components/products/product-type'
+import ProductCarousal from '@/components/products/products-carousal'
+import Reviews from '@/components/reviews/reviews'
+import Stars from '@/components/reviews/stars'
+import { Separator } from '@/components/ui/separator'
+import formatPrice from '@/lib/format-price'
+import { getReviewAverage } from '@/lib/review-average'
+import { db } from '@/server'
+import { productVariants } from '@/server/schema'
+import { eq } from 'drizzle-orm'
 
 export async function generateStaticParams() {
   const data = await db.query.productVariants.findMany({
@@ -18,12 +18,12 @@ export async function generateStaticParams() {
       product: true,
     },
     orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
-  });
+  })
   if (data) {
-    const slugID = data.map(variant => ({ slug: variant.id.toString() }));
-    return slugID;
+    const slugID = data.map(variant => ({ slug: variant.id.toString() }))
+    return slugID
   }
-  return [];
+  return []
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
@@ -39,26 +39,26 @@ export default async function Page({ params }: { params: { slug: string } }) {
         },
       },
     },
-  });
+  })
   if (variant) {
-    const reviewAvg = getReviewAverage(variant?.product?.reviews.map(review => review.rating));
+    const reviewAvg = getReviewAverage(variant?.product?.reviews.map(review => review.rating))
     return (
       <main>
-        <section className='flex flex-col gap-4 lg:flex-row lg:gap-12'>
-          <div className='flex-1'>
+        <section className="flex flex-col gap-4 lg:flex-row lg:gap-12">
+          <div className="flex-1">
             <ProductCarousal variants={variant.product.productVariants} />
           </div>
-          <div className='flex flex-1 flex-col'>
-            <h2 className='text-2xl font-bold'>{variant?.product?.title}</h2>
+          <div className="flex flex-1 flex-col">
+            <h2 className="text-2xl font-bold">{variant?.product?.title}</h2>
             <div>
               <ProductType variants={variant.product.productVariants} />
               <Stars rating={reviewAvg} totalReviews={variant.product.reviews.length} />
             </div>
-            <Separator className='my-2' />
-            <p className='py-2 text-2xl font-medium'>{formatPrice(variant.product.price)}</p>
+            <Separator className="my-2" />
+            <p className="py-2 text-2xl font-medium">{formatPrice(variant.product.price)}</p>
             <div dangerouslySetInnerHTML={{ __html: variant.product.description }}></div>
-            <p className='my-2 font-medium text-secondary-foreground'>Available Variants</p>
-            <div className='flex gap-4'>
+            <p className="my-2 font-medium text-secondary-foreground">Available Variants</p>
+            <div className="flex gap-4">
               {variant.product.productVariants.map(prodVariant => (
                 <ProductPicks
                   key={prodVariant.id}
@@ -76,6 +76,6 @@ export default async function Page({ params }: { params: { slug: string } }) {
         </section>
         <Reviews productID={variant.productID} />
       </main>
-    );
+    )
   }
 }
