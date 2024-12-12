6218d60f1050f974ae1174044b4392653c34ebc3
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-12 18:56:52 +0300

    Refactor FormError component: standardize import quotes, improve formatting, and update function declaration

--- components/auth/form-error.tsx
+++ components/auth/form-error.tsx
@@ -1,12 +1,13 @@
-import { AlertCircle } from 'lucide-react';
+import { AlertCircle } from 'lucide-react'
 
-export const FormError = ({ message }: { message?: string }) => {
-  if (!message) return null;
+export function FormError({ message }: { message?: string }) {
+  if (!message)
+    return null
 
   return (
-    <div className='rounded-md bg-destructive/30 my-2 text-xs font-medium p-3 text-secondary-foreground flex items-center gap-2'>
-      <AlertCircle className='mr-2 h-5 w-5' />
+    <div className="rounded-md bg-destructive/30 my-2 text-xs font-medium p-3 text-secondary-foreground flex items-center gap-2">
+      <AlertCircle className="mr-2 h-5 w-5" />
       <p>{message}</p>
     </div>
-  );
-};
+  )
+}
