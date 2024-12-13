d17b76b2257697fd588b87b1ea6859f1b805038d
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-13 16:54:14 +0300

    Refactor DashboardNav component: standardize import quotes, improve formatting, and update function declarations

--- components/navigation/dashboard-nav.tsx
+++ components/navigation/dashboard-nav.tsx
@@ -1,23 +1,23 @@
-'use client';
+'use client'
 
-import { cn } from '@/lib/utils';
-import { AnimatePresence, motion } from 'framer-motion';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils'
+import { AnimatePresence, motion } from 'framer-motion'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function DashboardNav({
   allLinks,
 }: {
   allLinks: {
-    label: string;
-    path: string;
-    icon: React.ReactNode;
-  }[];
+    label: string
+    path: string
+    icon: React.ReactNode
+  }[]
 }) {
-  const pathname = usePathname();
+  const pathname = usePathname()
   return (
-    <nav className='overflow-auto py-2 mb-4'>
-      <ul className='flex gap-6 text-xs font-semibold'>
+    <nav className="overflow-auto py-2 mb-4">
+      <ul className="flex gap-6 text-xs font-semibold">
         <AnimatePresence>
           {allLinks.map(link => (
             <motion.li key={link.path} whileTap={{ scale: 0.95 }}>
@@ -30,20 +30,22 @@ export default function DashboardNav({
               >
                 {link.icon}
                 {link.label}
-                {pathname === link.path ? (
-                  <motion.div
-                    initial={{ scale: 0.5 }}
-                    animate={{ scale: 1 }}
-                    layoutId='nav-underline'
-                    transition={{ type: 'spring', stiffness: 500, damping: 30 }}
-                    className='-bottom-1 absolute left-0 z-0 h-[2px] w-full rounded-full bg-primary'
-                  />
-                ) : null}
+                {pathname === link.path
+                  ? (
+                      <motion.div
+                        initial={{ scale: 0.5 }}
+                        animate={{ scale: 1 }}
+                        layoutId="nav-underline"
+                        transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+                        className="-bottom-1 absolute left-0 z-0 h-[2px] w-full rounded-full bg-primary"
+                      />
+                    )
+                  : null}
               </Link>
             </motion.li>
           ))}
         </AnimatePresence>
       </ul>
     </nav>
-  );
+  )
 }
