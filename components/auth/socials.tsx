1e5445a3a257edd514807fbbcde491c2e4738cec
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-13 16:53:50 +0300

    Refactor Socials component: standardize import quotes, improve formatting, and update function declarations

--- components/auth/socials.tsx
+++ components/auth/socials.tsx
@@ -1,37 +1,37 @@
-'use client';
+'use client'
 
-import { signIn } from 'next-auth/react';
-import { Button } from '../ui/button';
-import { FcGoogle } from 'react-icons/fc';
-import { BsTwitterX } from "react-icons/bs";
+import { signIn } from 'next-auth/react'
+import { BsTwitterX } from 'react-icons/bs'
+import { FcGoogle } from 'react-icons/fc'
+import { Button } from '../ui/button'
 
 export default function Socials() {
   return (
-    <div className='flex flex-col items-center w-full gap-4'>
+    <div className="flex flex-col items-center w-full gap-4">
       <Button
-      variant={'outline'}
-      className='flex gap-4 w-full'
+        variant="outline"
+        className="flex gap-4 w-full"
         onClick={() =>
           signIn('google', {
             redirect: false,
             redirectTo: '/',
-          })
-        }
+          })}
       >
         <span>Sign in with Google</span>
-        <FcGoogle className='w-5 h-5'/>
+        <FcGoogle className="w-5 h-5" />
       </Button>
-      <Button className='flex gap-4 w-full' 
-      variant={'outline'}
-      onClick={() =>
-        signIn('twitter', {
-          redirect: false,
-          redirectTo: '/',
-        })
-      }>
+      <Button
+        className="flex gap-4 w-full"
+        variant="outline"
+        onClick={() =>
+          signIn('twitter', {
+            redirect: false,
+            redirectTo: '/',
+          })}
+      >
         <span>Sign in with </span>
-        <BsTwitterX className='w-5 h-5'/>
+        <BsTwitterX className="w-5 h-5" />
       </Button>
     </div>
-  );
+  )
 }
