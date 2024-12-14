8a3ca2a6cd7829a7c544e563a0b3d94a0080b2fb
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-14 16:56:17 +0300

    Refactor Nav component: improve formatting, standardize quotes, and integrate CartDrawer for cart management

--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,43 +1,50 @@
-import { auth } from '@/server/auth';
-import { UserBtn } from '@/components/navigation/userBtn';
-import { Button } from '../ui/button';
-import Link from 'next/link';
 // import { LogIn } from 'lucide-react';
-import Logo from '@/assets/images/logo.webp';
-import Image from 'next/image';
+import Logo from '@/assets/images/logo.webp'
+import { UserBtn } from '@/components/navigation/userBtn'
+import { auth } from '@/server/auth'
+import Image from 'next/image'
+import Link from 'next/link'
+import CartDrawer from '../cart/cart-drawer'
+import { Button } from '../ui/button'
 
 export default async function Nav() {
-  const session = await auth();
-  console.log(session);
+  const session = await auth()
+  console.log(session)
 
   return (
-    <header className='py-8'>
+    <header className="py-8">
       <nav>
-        <ul className='flex justify-between items-center'>
-          <li>
-            <Link href='/' aria-label='drip'>
-            <div className='h-15 w-15 pb-3'>
-              <Image src={Logo} alt='Logo' className='size-12' />
+        <ul className="flex justify-between items-center md:gap-8 gap-4">
+          <li className="flex flex-1">
+            <Link href="/" aria-label="drip">
+              <div className="h-15 w-15 pb-3">
+                <Image src={Logo} alt="Logo" className="size-12" />
 
-            </div>
+              </div>
             </Link>
           </li>
-          {!session ? (
-            <li>
-              <Button asChild>
-                <Link href={'/auth/login'} aria-label='sign-in' className='flex gap-3'>
-                  {/* <LogIn size={13} /> */}
-                  <span>Login</span>
-                </Link>
-              </Button>
-            </li>
-          ) : (
-            <li>
-              <UserBtn expires={session?.expires ?? ''} user={session?.user} />
-            </li>
-          )}
+          <li className="relative flex items-center hover:bg-muted">
+            <CartDrawer />
+          </li>
+          {
+            !session
+              ? (
+                  <li className="flex items-center justify-center">
+                    <Button asChild>
+                      <Link href="/auth/login" aria-label="sign-in" className="flex gap-3">
+                        {/* <LogIn size={13} /> */}
+                        <span>Login</span>
+                      </Link>
+                    </Button>
+                  </li>
+                ) : (
+                  <li className="flex items-center justify-center">
+                    <UserBtn expires={session?.expires ?? ''} user={session?.user} />
+                  </li>
+                )
+          }
         </ul>
       </nav>
     </header>
-  );
+  )
 }
