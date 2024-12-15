cb567dbfc740e4d46fe173c28c2375666eeec0e1
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-15 16:58:20 +0300

    Add AddCart component to product page for enhanced cart functionality

--- components/cart/cart-drawer.tsx
+++ components/cart/cart-drawer.tsx
@@ -1,14 +1,37 @@
 'use client'
 
-import { useCardStore } from '@/lib/client-store'
+import { useCartStore } from '@/lib/client-store'
+import { AnimatePresence, motion } from 'framer-motion'
 import { ShoppingBag } from 'lucide-react'
+import { Drawer, DrawerContent, DrawerHeader, DrawerTrigger } from '../ui/drawer'
 
 export default function CartDrawer() {
-  const { cart } = useCardStore()
+  const { cart } = useCartStore()
 
   return (
-    <div>
-      <ShoppingBag />
-    </div>
+    <Drawer>
+      <DrawerTrigger>
+        <div className="relative px-2">
+          <AnimatePresence>
+            {cart.length > 0 && (
+              <motion.span
+                initial={{ scale: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0 }}
+                className="absolute flex items-center justify-center -top-1 w-4 h-4 dark:bg-primary bg-primary text-white text-sm font-bold rounded-full -right-0.5"
+              >
+                {cart.length}
+              </motion.span>
+            )}
+          </AnimatePresence>
+          <ShoppingBag />
+        </div>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <h2 className="text-xl font-bold">Cart</h2>
+        </DrawerHeader>
+      </DrawerContent>
+    </Drawer>
   )
 }
