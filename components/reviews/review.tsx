47a388539a7cc29984dfb9cbcc09680674dd043a
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-16 16:58:49 +0300

    Refactor Review component: standardize quotes and improve code formatting

--- components/reviews/review.tsx
+++ components/reviews/review.tsx
@@ -1,38 +1,38 @@
-'use client';
+'use client'
 
-import { ReviewsWithUser } from '@/lib/infer-types';
-import { motion } from 'framer-motion';
-import { Card } from '../ui/card';
-import Image from 'next/image';
-import { formatDistance, subDays } from 'date-fns';
-import Stars from './stars';
+import type { ReviewsWithUser } from '@/lib/infer-types'
+import { formatDistance, subDays } from 'date-fns'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import { Card } from '../ui/card'
+import Stars from './stars'
 
 export default function Review({ reviews }: { reviews: ReviewsWithUser[] }) {
   return (
-    <motion.div className='flex flex-col gap-4'>
+    <motion.div className="flex flex-col gap-4">
       {reviews.map(review => (
-        <Card key={review.id} className='p-4'>
-          <div className='flex items-center gap-2'>
+        <Card key={review.id} className="p-4">
+          <div className="flex items-center gap-2">
             <Image
-              className='rounded-full'
+              className="rounded-full"
               src={review.user.image!}
               alt={review.user.name!}
               width={32}
               height={32}
             />
             <div>
-              <p className='text-sm font-bold'>{review.user.name}</p>
-              <div className='flex items-center gap-2'>
+              <p className="text-sm font-bold">{review.user.name}</p>
+              <div className="flex items-center gap-2">
                 <Stars rating={review.rating} />
-                <p className='text-bold text-xs text-muted-foreground'>
+                <p className="text-bold text-xs text-muted-foreground">
                   {formatDistance(subDays(review.created!, 0), new Date())}
                 </p>
               </div>
             </div>
           </div>
-          <p className='py-2 font-medium'>{review.comment}</p>
+          <p className="py-2 font-medium">{review.comment}</p>
         </Card>
       ))}
     </motion.div>
-  );
+  )
 }
