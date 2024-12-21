7c7f9618dcc6838015b1f17955764cef96a8ada9
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-21 17:30:09 +0300

    Refactor auth.ts: streamline imports and improve code readability

--- server/auth.ts
+++ server/auth.ts
@@ -1,18 +1,19 @@
-import NextAuth from 'next-auth';
-import dotenv from 'dotenv';
-import { DrizzleAdapter } from '@auth/drizzle-adapter';
-import { db } from '@/server';
-import Google from 'next-auth/providers/google';
-import Twitter from 'next-auth/providers/twitter';
-import Credentials from 'next-auth/providers/credentials';
-import { loginSchema } from '@/types/login-schema';
-import { eq } from 'drizzle-orm';
-import { accounts, users } from './schema';
-import bcrypt from 'bcrypt';
+import process from 'node:process'
+import { db } from '@/server'
+import { loginSchema } from '@/types/login-schema'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+import { eq } from 'drizzle-orm'
+import NextAuth from 'next-auth'
+import Credentials from 'next-auth/providers/credentials'
+import Google from 'next-auth/providers/google'
+import Twitter from 'next-auth/providers/twitter'
+import { accounts, users } from './schema'
 
 dotenv.config({
   path: '.env.local',
-});
+})
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
@@ -23,42 +24,44 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     async session({ session, token }) {
       if (session && token.sub) {
-        session.user.id = token.sub;
+        session.user.id = token.sub
       }
       if (session.user && token.role) {
-        session.user.role = token.role as string;
+        session.user.role = token.role as string
       }
       if (session.user) {
-        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
-        session.user.name = token.name as string;
-        session.user.email = token.email as string;
-        session.user.isOAuth = token.isOAuth as boolean;
-        session.user.image = token.image as string;
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
+        session.user.name = token.name as string
+        session.user.email = token.email as string
+        session.user.isOAuth = token.isOAuth as boolean
+        session.user.image = token.image as string
       }
 
-      return session;
+      return session
     },
     async jwt({ token }) {
-      if (!token.sub) return token;
+      if (!token.sub)
+        return token
 
       const exsitingUser = await db.query.users.findFirst({
         where: eq(users.id, token.sub),
-      });
+      })
 
-      if (!exsitingUser) return token;
+      if (!exsitingUser)
+        return token
 
       const exsitingAccount = await db.query.accounts.findFirst({
         where: eq(accounts.userId, exsitingUser.id),
-      });
+      })
 
-      token.isOAuth = !!exsitingAccount;
-      token.name = exsitingUser.name;
-      token.email = exsitingUser.email;
-      token.role = exsitingUser.role;
-      token.isTwoFactorEnabled = exsitingUser.twoFactorEnabled;
-      token.image = exsitingUser.image;
+      token.isOAuth = !!exsitingAccount
+      token.name = exsitingUser.name
+      token.email = exsitingUser.email
+      token.role = exsitingUser.role
+      token.isTwoFactorEnabled = exsitingUser.twoFactorEnabled
+      token.image = exsitingUser.image
 
-      return token;
+      return token
     },
   },
   providers: [
@@ -75,24 +78,26 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       allowDangerousEmailAccountLinking: true,
     }),
     Credentials({
-      authorize: async credentials => {
-        const validatedFields = loginSchema.safeParse(credentials);
+      authorize: async (credentials) => {
+        const validatedFields = loginSchema.safeParse(credentials)
         if (!validatedFields.success || !validatedFields.data) {
-          return null;
+          return null
         }
-        const { email, password } = validatedFields.data;
+        const { email, password } = validatedFields.data
         const user = await db.query.users.findFirst({
           where: eq(users.email, email),
-        });
+        })
 
-        if (!user || !user.password) return null;
+        if (!user || !user.password)
+          return null
 
-        const passwordMatch = await bcrypt.compare(password, user.password);
+        const passwordMatch = await bcrypt.compare(password, user.password)
 
-        if (!passwordMatch) return null;
+        if (!passwordMatch)
+          return null
 
-        return user;
+        return user
       },
     }),
   ],
-});
+})
