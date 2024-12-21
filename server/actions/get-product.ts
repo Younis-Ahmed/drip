8836972794f8b078c2245539d59ed9a75c4aeea1
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-21 17:31:51 +0300

    Refactor get-product.ts: streamline imports and improve error handling

--- server/actions/get-product.ts
+++ server/actions/get-product.ts
@@ -1,19 +1,20 @@
-'use server';
+'use server'
 
-import { eq } from 'drizzle-orm';
-import { products } from '../schema';
-import { db } from '..';
+import { eq } from 'drizzle-orm'
+import { db } from '..'
+import { products } from '../schema'
 
 export async function getProduct(id: number) {
   try {
     const product = await db.query.products.findFirst({
       where: eq(products.id, id),
-    });
+    })
     if (!product) {
-      return { error: 'Product not found' };
+      return { error: 'Product not found' }
     }
-    return { success: product };
-  } catch (error) {
-    return { error: `Failed to get product\nerror: ${error}` };
+    return { success: product }
+  }
+  catch (error) {
+    return { error: `Failed to get product\nerror: ${error}` }
   }
 }
