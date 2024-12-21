2e554dd87bcf565f1be7fc398cc763aa957263ef
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-21 17:32:06 +0300

    Refactor settings.ts: streamline imports and improve error handling

--- server/actions/settings.ts
+++ server/actions/settings.ts
@@ -1,45 +1,50 @@
-'use server';
+'use server'
 
-import { SettingsSchema } from '@/types/settings-schema';
-import { createSafeActionClient } from 'next-safe-action';
-import { auth } from '../auth';
-import { db } from '..';
-import { eq } from 'drizzle-orm';
-import { users } from '../schema';
-import bcrypt from 'bcrypt';
-import { revalidatePath } from 'next/cache';
+import { SettingsSchema } from '@/types/settings-schema'
+import bcrypt from 'bcrypt'
+import { eq } from 'drizzle-orm'
+import { createSafeActionClient } from 'next-safe-action'
+import { revalidatePath } from 'next/cache'
+import { db } from '..'
+import { auth } from '../auth'
+import { users } from '../schema'
 
-const action = createSafeActionClient();
+const action = createSafeActionClient()
 
 export const Settings = action.schema(SettingsSchema).action(async ({ parsedInput }) => {
-  const user = await auth();
-  if (!user) return { error: 'User not found' };
+  const user = await auth()
+  if (!user)
+    return { error: 'User not found' }
   const dbUser = await db.query.users.findFirst({
     where: eq(users.id, user.user.id),
-  });
-  if (!dbUser) return { error: 'User not found' };
+  })
+  if (!dbUser)
+    return { error: 'User not found' }
 
   if (user.user.isOAuth) {
-    parsedInput.email = undefined;
-    parsedInput.password = undefined;
-    parsedInput.newPassword = undefined;
-    parsedInput.isTwoFactorEnabled = undefined;
+    parsedInput.email = undefined
+    parsedInput.password = undefined
+    parsedInput.newPassword = undefined
+    parsedInput.isTwoFactorEnabled = undefined
   }
 
   if (parsedInput.password && parsedInput.newPassword && dbUser.password) {
-    const passwordMatch = await bcrypt.compare(parsedInput.password, dbUser.password);
-    if (!passwordMatch) return { error: 'Invalid password' };
+    const passwordMatch = await bcrypt.compare(parsedInput.password, dbUser.password)
+    if (!passwordMatch)
+      return { error: 'Invalid password' }
 
-    const savePassword = await bcrypt.compare(parsedInput.newPassword, dbUser.password);
+    const savePassword = await bcrypt.compare(parsedInput.newPassword, dbUser.password)
 
-    if (savePassword) return { error: 'New password must be different' };
+    if (savePassword)
+      return { error: 'New password must be different' }
 
-    const hashedPassword = await bcrypt.hash(parsedInput.newPassword, 10);
+    const hashedPassword = await bcrypt.hash(parsedInput.newPassword, 10)
 
-    parsedInput.password = hashedPassword;
-    parsedInput.newPassword = undefined;
+    parsedInput.password = hashedPassword
+    parsedInput.newPassword = undefined
   }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+  // eslint-disable-next-line unused-imports/no-unused-vars
   const updatedUser = await db
     .update(users)
     .set({
@@ -49,8 +54,8 @@ export const Settings = action.schema(SettingsSchema).action(async ({ parsedInpu
       email: parsedInput.email,
       image: parsedInput.image,
     })
-    .where(eq(users.id, dbUser.id));
-  revalidatePath('/dashboard/settings');
+    .where(eq(users.id, dbUser.id))
+  revalidatePath('/dashboard/settings')
 
-  return { success: 'Settings updated' };
-});
+  return { success: 'Settings updated' }
+})
