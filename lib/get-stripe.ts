2591cf4547b260d8154ce1e2ede5f11d2dbabcf0
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-24 17:46:23 +0300

    Add getStripe utility for loading Stripe instance

---
+++ lib/get-stripe.ts
@@ -0,0 +1,14 @@
+import type { Stripe } from '@stripe/stripe-js'
+import process from 'node:process'
+import { loadStripe } from '@stripe/stripe-js'
+
+let stripePromise: Promise<Stripe | null>
+
+function getStripe() {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
+  }
+  return stripePromise
+}
+
+export default getStripe
