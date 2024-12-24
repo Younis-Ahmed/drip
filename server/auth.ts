103641b5e2106533972871c22d12ebfdcded0b03
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-24 17:46:34 +0300

    Add Stripe customer creation on user registration

--- server/auth.ts
+++ server/auth.ts
@@ -9,6 +9,7 @@ import NextAuth from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
 import Google from 'next-auth/providers/google'
 import Twitter from 'next-auth/providers/twitter'
+import Stripe from 'stripe'
 import { accounts, users } from './schema'
 
 dotenv.config({
@@ -20,6 +21,19 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET,
   session: {
     strategy: 'jwt',
+  },
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+        apiVersion: '2024-12-18.acacia',
+      })
+
+      const customer = await stripe.customers.create({
+        email: user.email!,
+        name: user.name!,
+      })
+      await db.update(users).set({ customerID: customer.id }).where(eq(users.id, user.id!))
+    },
   },
   callbacks: {
     async session({ session, token }) {
