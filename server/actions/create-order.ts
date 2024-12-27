0d04531ddddef0ee8379e20a45dc24288fdcd8e6
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2024-12-27 16:06:07 +0300

    Implement order creation logic with user authentication and product mapping

--- server/actions/create-order.ts
+++ server/actions/create-order.ts
@@ -2,9 +2,32 @@
 
 import { orderSchema } from '@/types/order-schema'
 import { createSafeActionClient } from 'next-safe-action'
+import { db } from '..'
+import { auth } from '../auth'
+import { orderProduct, orders } from '../schema'
 
 const action = createSafeActionClient()
 
 export const createOrder = action
   .schema(orderSchema)
-  .action(async ({ parsedInput }) => {})
+  .action(async ({ parsedInput: { products, status, total } }) => {
+    const user = await auth()
+    if (!user)
+      return { error: 'You must be logged in to create an order' }
+
+    const order = await db.insert(orders).values({
+      status,
+      total,
+      userID: user.user.id,
+    }).returning()
+
+    const orderProducts = products.map(async ({ productID, quantity, variantID }) => {
+      const NewOrderProducts = await db.insert(orderProduct).values({
+        quantity,
+        orderID: order[0].id,
+        productID,
+        productVariantsID: variantID,
+      })
+    })
+    return { success: `Order ${order[0].id} has been created` }
+  })
