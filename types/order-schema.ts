38694b5ab6479f64dd91d00a2d989739042eaff5
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-01-02 17:04:21 +0300

    Add paymentIntentId field to order schema

--- types/order-schema.ts
+++ types/order-schema.ts
@@ -3,6 +3,7 @@ import * as z from 'zod'
 export const orderSchema = z.object({
   total: z.number(),
   status: z.enum(['pending', 'completed', 'cancelled']),
+  paymentIntentId: z.string(),
   products: z.array(
     z.object({
       quantity: z.number(),
