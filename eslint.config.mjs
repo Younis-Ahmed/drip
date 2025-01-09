55ed0334380d1fd9daec61f9f322cdb18dc9293f
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-01-09 18:34:15 +0300

    chore: Change no-console rule from off to warn in ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,8 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   rules: {
-    'no-console': 'off',
-    'unicorn/prefer-node-protocol': 'off',
+    'no-console': 'warn',
+    'unicorn/prefer-node-protocol': 'off', // Next.js's webpack doesn't support node: protocol
   },
   ignores: ['dist', 'node_modules', 'tailwind.config.ts'],
   extends: ['next/core-web-vitals', 'next/typescript'],
