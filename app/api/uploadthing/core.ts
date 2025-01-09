fd2b964df979ac2c89c6bba48a52cb10ac491a8c
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-01-09 18:34:55 +0300

    feat: Enhance file upload functionality with error handling

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,26 +1,27 @@
+/* eslint-disable unused-imports/no-unused-vars */
+import { metadata } from '@/app/layout'
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
-// import { UploadThingError } from 'uploadthing/server';
+import { UploadThingError } from 'uploadthing/server'
 
 const f = createUploadthing()
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  avatarUploader: f({ image: { maxFileSize: '2MB' } })
-    // This code runs on your server before upload
-    .onUploadComplete(async ({ metadata, file }) => {
-      //   // This code RUNS ON YOUR SERVER after upload
-      console.info('Upload complete for userId:', metadata)
-      console.info('file url', file.url)
-      //   // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      //   return { uploadedBy: metadata.userId };
-    }),
-  variantUploader: f({ image: { maxFileSize: '4MB', maxFileCount: 10 } }).onUploadComplete(
-    async ({ metadata, file }) => {
-      console.log(file)
-      console.log(metadata)
-    },
+  avatarUploader: f({ image: { maxFileSize: '2MB' } }).onUploadComplete(
+    async ({ metadata, file }) => {},
   ),
+
+  variantUploader: f({
+    image: { maxFileCount: 10, maxFileSize: '4MB' },
+  })
+    .onUploadError(async ({ error }) => {
+      console.log(error)
+      console.log('errorrr')
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log(file)
+    }),
 } satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
